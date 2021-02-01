dddd702a68e4e6f3899c53676be6a7ed46341335
dina-sour <matveev.dina@gmail.com>
2021-02-01 21:29:43 +0200

    project skeleton

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <AppContainer>
+      <p>Hello</p>
+    </AppContainer>
+  );
+}
+
+const AppContainer = styled.div`
+  display: flex;
+`;
+
+export default App;
