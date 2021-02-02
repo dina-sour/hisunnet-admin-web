7353cd1faa7cd256cbd7b7d4bf0d0beffc5dc39a
dina-sour <matveev.dina@gmail.com>
2021-02-02 18:46:15 +0200

    project structure + started making form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import './index.css';
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+import ClinicDetailsPage from "./pages/clinic-details/ClinicDetailsPage";
 
 function App() {
   return (
     <AppContainer>
-      <p>Hello</p>
+      <Header>
+        <ClinicInfoGroup>
+          <MaccabiIcon>M</MaccabiIcon>
+        </ClinicInfoGroup>
+      </Header>
+      <ClinicDetailsPage />
     </AppContainer>
   );
 }
 
 const AppContainer = styled.div`
   display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  height: 100px;
+  width: 100%;
+  background: #d6e2f1;
+  display: flex;
+  flex-direction: row;
+`;
+
+const ClinicInfoGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-self: flex-end;
+  width: 357px;
+`;
+
+const MaccabiIcon = styled(Avatar)`
+  && {
+    height: 40px;
+    width: 40px;
+    align-self: flex-end;
+  }
 `;
 
 export default App;
---
+++ src/pages/clinic-details/ClinicDetailsPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ClinicDetailsPage = () => {
+  return (
+    <Container>
+      <p>Hello</p>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+`;
+
+export default ClinicDetailsPage;
