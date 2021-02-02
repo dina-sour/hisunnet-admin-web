6b6f90129ab11f700f4e340a5219bd68de00ea53
dina-sour <matveev.dina@gmail.com>
2021-02-02 23:32:14 +0200

    styling header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './index.css';
+import "./index.css";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
 import ClinicDetailsPage from "./pages/clinic-details/ClinicDetailsPage";
@@ -8,7 +8,9 @@ function App() {
   return (
     <AppContainer>
       <Header>
+        <p>שם משתמש כלשהו</p>
         <ClinicInfoGroup>
+          <p>מתחם חיסונים בתל אביב</p>
           <MaccabiIcon>M</MaccabiIcon>
         </ClinicInfoGroup>
       </Header>
@@ -28,13 +30,14 @@ const Header = styled.div`
   background: #d6e2f1;
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
 `;
 
 const ClinicInfoGroup = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: flex-start;
-  align-self: flex-end;
+  justify-content: flex-end;
+  align-items: center;
   width: 357px;
 `;
 
@@ -42,7 +45,6 @@ const MaccabiIcon = styled(Avatar)`
   && {
     height: 40px;
     width: 40px;
-    align-self: flex-end;
   }
 `;
 
--- src/pages/clinic-details/ClinicDetailsPage.js
+++ src/pages/clinic-details/ClinicDetailsPage.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 const ClinicDetailsPage = () => {
   return (
     <Container>
-      <p>Hello</p>
     </Container>
   );
 }
