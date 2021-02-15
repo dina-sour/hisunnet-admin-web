3c0452756bf45d7ea33bc646a9e03d3b62f59d55
dina-sour <matveev.dina@gmail.com>
2021-02-15 23:36:50 +0200

    working on user info group

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,23 @@
 import React from "react";
 import styled from "styled-components";
-import { Avatar, Accordion } from "@material-ui/core";
-import Clock from 'react-live-clock';
+import { Avatar, Accordion, Divider, Button } from "@material-ui/core";
+import Clock from "react-live-clock";
 
 const Header = (props) => {
   return (
     <Container>
+      <UserInfoGroup>
+        <LiveClock ticking={true} />
+        <GroupDivider orientation="vertical" />
+        <LogoutButton>התנתקות</LogoutButton>
+        <GroupDivider orientation="vertical" />
         <CurrentUser>{props.userName}</CurrentUser>
-        <ClinicInfoGroup>
-          <ClinicName>מרפאה לחיסוני קורונה -  {props.area}</ClinicName>
-          <ClinicIcon />
-          <Clock format={'HH:MM'} ticking={true} timezone={'US/Pacific'} />
-        </ClinicInfoGroup>
+        <UserIcon />
+      </UserInfoGroup>
+      <ClinicInfoGroup>
+        <ClinicName>מרפאה לחיסוני קורונה - {props.area}</ClinicName>
+        <ClinicIcon />
+      </ClinicInfoGroup>
     </Container>
   );
 };
@@ -32,14 +38,22 @@ const ClinicInfoGroup = styled.div`
   justify-content: flex-start;
   align-items: center;
   width: 357px;
+  margin-right: 50px;
 `;
 
 const ClinicIcon = styled.svg`
   && {
-    height: 40px;
-    width: 40px;
-    margin: 37px 40px 23px 20px;
+    height: 29px;
+    width: 75px;
     background-image: url(${(props) => props.theme.clinicIcon});
+  }
+`;
+
+const GroupDivider = styled(Divider)`
+  && {
+    align-self: center;
+    background-color: white;
+    height: 24px;
   }
 `;
 
@@ -48,8 +62,52 @@ const ClinicName = styled.div`
   font-size: 18px;
 `;
 
-const CurrentUser = styled.div`
-  margin: 50px 0 29px 40.5px;
+const UserInfoGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 45px;
+  width: 298px;
+  align-self: center;
+  justify-content: space-between;
+  align-items: center;
 `;
 
+const LiveClock = styled(Clock)`
+  color: #ffffff;
+  font-family: Heebo;
+  font-size: 14px;
+  letter-spacing: 0;
+  line-height: 21px;
+`;
+
+const CurrentUser = styled.div`
+  height: 21px;
+  width: 53px;
+  color: #ffffff;
+  font-family: Heebo;
+  font-size: 14px;
+  letter-spacing: 0;
+  line-height: 21px;
+  text-align: center;
+`;
+
+const UserIcon = styled(Avatar)`
+  height: 45px;
+  width: 45px;
+  border-radius: 100px;
+  background-color: white;
+`;
+
+const LogoutButton = styled(Button)`
+  && {
+    color: white;
+    width: 119px;
+    color: #ffffff;
+    font-family: Heebo;
+    font-size: 14px;
+    letter-spacing: 0;
+    line-height: 21px;
+    font-weight: 250;
+  }
+`;
 export default Header;
